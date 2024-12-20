5da46e4350177dd287c8154a89f1b2c4bdfee532
NaMinhyeok <nmh9097@gmail.com>
2024-12-20 12:44:09 +0900

    feat: 투표 스케줄링 생성

--- src/main/kotlin/com/yeoljeong/autovote/domain/poll/PollService.kt
+++ src/main/kotlin/com/yeoljeong/autovote/domain/poll/PollService.kt
@@ -2,7 +2,10 @@ package com.yeoljeong.autovote.domain.poll
 
 import com.yeoljeong.autovote.application.api.poll.request.PollRegisterRequest
 import com.yeoljeong.autovote.domain.user.UserReader
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.time.temporal.ChronoField
 
 @Service
 class PollService(
@@ -20,6 +23,44 @@ class PollService(
             userId = user.id
         )
         pollAppender.append(poll)
+    }
+
+    @Scheduled(cron = "0 0 9 * * SAT")
+    fun bookPoll() {
+        val now = LocalDateTime.now()
+        val weekOfMonth = now.get(ChronoField.ALIGNED_WEEK_OF_MONTH)
+        val month = now.monthValue
+        val title = "${month}월 ${weekOfMonth}주차 투표"
+
+        val endAt = now.plusDays(3)
+            .withHour(12)
+            .withMinute(0)
+            .withSecond(0)
+
+        // 옵션 생성
+        val friday = now.plusDays(6)  // 다음주 금요일
+        val saturday = now.plusDays(7)  // 다음주 토요일
+
+        val options = listOf(
+            "${friday.monthValue}/${friday.dayOfMonth} 금요일 20시~22시",
+            "${friday.monthValue}/${friday.dayOfMonth} 금요일 21시~23시",
+            "${friday.monthValue}/${friday.dayOfMonth} 금요일 22시~24시",
+            "${saturday.monthValue}/${saturday.dayOfMonth} 토요일 20시~22시",
+            "${saturday.monthValue}/${saturday.dayOfMonth} 토요일 21시~23시",
+            "${saturday.monthValue}/${saturday.dayOfMonth} 토요일 22시~24시",
+            "불참"
+        )
+
+        val poll = Poll.register(
+            title = title,
+            description = "정기 풋살 투표",
+            endAt = endAt,
+            options = options,
+            userId = 1
+        )
+
+        pollAppender.append(poll)
+
     }
 
 }
---
+++ src/main/kotlin/com/yeoljeong/autovote/infrastructure/scheduled/SchedulingConfig.kt
@@ -0,0 +1,9 @@
+package com.yeoljeong.autovote.infrastructure.scheduled
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@EnableScheduling
+@Configuration
+class SchedulingConfig {
+}
